e3269d87de9211b6334d29e0f417d86822051329
ccxbox <pierrelouis.thevenard@gmail.com>
2025-12-27 18:05:56 +0100

    amélioration,carte,vidéos...

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function loadPage(url) {
     .then(response => response.text())
     .then(html => {
       main.innerHTML = html;
+      // Scroll to top of the main content area
+      main.scrollTop = 0;
+      // Or scroll the page to the top of main
+      main.scrollIntoView({ behavior: 'smooth', block: 'start' });
     })
     .catch(error => {
       main.innerHTML = '<p>Sorry, could not load this page.</p>';
